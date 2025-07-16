9a0db0e848ea3c373a0dc88c4aefc6255c27c179
Silvia Al Hafoudh <silvia.alhafoudh@magnepelia.com>
2025-07-16 08:52:41 +0200

    Add styling to notes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
  validates :name, presence: true
- validate :expected_cost,  comparison: { less_than: 50 }
+ validates :expected_cost,  comparison: { less_than: 50 }
 # validates :is_done, acceptance: true, on: :create
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,5 @@
 class Note < ApplicationRecord
   
-  validates :name, lenght: { minimum: 3 }
+  validates :name, length: { minimum: 3 }
   validates :body, presence: true
 end
